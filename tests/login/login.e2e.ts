efaf4378234af7f6908fa1143aa9d0294fadc7c2
shahbaz844 <56486185+shahbaz844@users.noreply.github.com>
2024-04-14 11:48:07 +0500

    Update login.e2e.ts

--- tests/login/login.e2e.ts
+++ tests/login/login.e2e.ts
@@ -1,5 +1,5 @@
 import { LoginPage } from '../../pageObjects/login/login.page';
-import {HomePage} from "../../pageObjects/home/home.page";
+import { HomePage } from "../../pageObjects/home/home.page";
 import allureReporter from "@wdio/allure-reporter"
 
 describe('Login application', () => {
@@ -7,12 +7,9 @@ describe('Login application', () => {
     const homePage = new HomePage();
     const loginPage = new LoginPage();
 
-    before(async () => {
+    it('should login with valid credentials', async () => {
         await loginPage.openLoginPage();
         await homePage.clickOnSignIn();
-    })
-
-    it('should login with valid credentials', async () => {
         allureReporter.addSeverity("Critical")
         await loginPage.performLogin(process.env.USERNAME, process.env.PASSWORD);
         await browser.pause(5000);
